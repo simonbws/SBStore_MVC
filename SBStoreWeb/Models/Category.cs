89827ed13aa4966f5517a97d996dde76ea3048a5
simonbws <simonbws6@gmail.com>
2024-02-21 23:21:05 +0100

    ConnectionString and NuGet packages

--- SBStoreWeb/Models/Category.cs
+++ SBStoreWeb/Models/Category.cs
@@ -1,9 +1,14 @@
-﻿namespace SBStoreWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SBStoreWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }    
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
 
     }
 }
