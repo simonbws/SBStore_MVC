6cb5deadc51d6079562d301ab504b6377c419b49
simonbws <simonbws6@gmail.com>
2024-02-25 14:25:10 +0100

    Setup AppDbContext with SQLServer Connection

---
+++ SBStoreWeb/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SBStoreWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+               
+        }
+    }
+}
