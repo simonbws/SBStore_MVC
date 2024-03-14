e24ba419a10e31da1348d1f4a88d7ba319996ee7
simonbws <simonbws6@gmail.com>
2024-03-15 00:44:20 +0100

    Repository Interface and it's Implementation

---
+++ SBStoreDataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SBStore.DataAccess.Data;
+using SBStore.DataAccess.Repository.IRepository;
+namespace SBStore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _data;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext data)
+        {
+            _data = data;
+            this.dbSet = _data.Set<T>();
+            
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+           dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+    }
+}
