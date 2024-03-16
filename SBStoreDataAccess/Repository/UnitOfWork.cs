c7af3853562529f438a50e699cd49f680542a73c
simonbws <simonbws6@gmail.com>
2024-03-16 13:25:59 +0100

    UnitOfWork pattern for CategoryRepository /save method

--- SBStoreDataAccess/Repository/UnitOfWork.cs
+++ SBStoreDataAccess/Repository/UnitOfWork.cs
@@ -1,28 +1,27 @@
 ﻿using SBStore.DataAccess.Data;
 using SBStore.DataAccess.Repository.IRepository;
-using SBStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SBStore.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
+
         private AppDbContext _data;
-        public CategoryRepository(AppDbContext data) : base(data) 
+        public ICategoryRepository Category { get; private set; }
+        public UnitOfWork(AppDbContext data) 
         {
-              _data = data;
+            _data = data;
+            Category = new CategoryRepository(_data);
         }
-
-        
-
-        public void Update(Category obj)
+       
+        public void Save()
         {
-            _data.Categories.Update(obj);
+            _data.SaveChanges();
         }
     }
 }
