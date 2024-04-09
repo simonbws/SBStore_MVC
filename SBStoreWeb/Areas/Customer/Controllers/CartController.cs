0cbee4be1b1d89d2e26a2ef42e8a0ed67a46404c
simonbws <simonbws6@gmail.com>
2024-04-09 23:05:13 +0200

    Get Shopping Cart Product Lists

--- SBStoreWeb/Areas/Customer/Controllers/CartController.cs
+++ SBStoreWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SBStore.DataAccess.Repository.IRepository;
+using SBStore.Models.ViewModels;
+using System.Security.Claims;
 
 namespace SBStoreWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
-        [Area("Customer")]
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.AppUserId == userId, includeProperties: "Product")
+            };
+            return View(ShoppingCartVM);
         }
     }
 }
