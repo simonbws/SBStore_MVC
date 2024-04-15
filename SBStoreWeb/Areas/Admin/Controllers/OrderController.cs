236a1d03636f5548378a3cc418feb60613c4e3c1
simonbws <simonbws6@gmail.com>
2024-04-16 00:23:22 +0200

    OrderVM, Controller and Order List

---
+++ SBStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using SBStore.DataAccess.Repository.IRepository;
+using SBStore.Models;
+
+namespace SBStoreWeb.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "AppUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+       
+
+
+        #endregion
+    }
+}
---
+++ SBStoreWeb/wwwroot/js/order.js
@@ -0,0 +1,28 @@
+﻿var datTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+   datTable = $('#tblData').DataTable({
+       "ajax": { url: "/admin/order/getall" },
+        "columns": [
+            { data: "id", "width": "25%" },
+            { data: "name", "width": "15%" },
+            { data: "phoneNumber", "width": "10%" },
+            { data: "appuser.email", "width": "15%" },
+            { data: "orderStatus", "width": "10%" },
+            { data: "orderTotal", "width": "10%" },
+            {
+                data: "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
