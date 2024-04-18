6039d96893ed66421d4c11a10e88b6c46bf87786
simonbws <simonbws6@gmail.com>
2024-04-18 20:21:00 +0200

    Loading Orders Details

--- SBStoreWeb/Areas/Admin/Controllers/OrderController.cs
+++ SBStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using SBStore.DataAccess.Repository.IRepository;
 using SBStore.Models;
+using SBStore.Models.ViewModels;
+using SBStore.Utility;
+using System.Diagnostics;
 
 namespace SBStoreWeb.Areas.Admin.Controllers
 {
@@ -20,13 +23,41 @@ namespace SBStoreWeb.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Details(int orderId)
+        {
+            OrderVM ordervm = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "AppUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+            return View(ordervm);
+        }
+
 
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "AppUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "AppUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
             return Json(new { data = objOrderHeaders });
         }
        
--- SBStoreWeb/wwwroot/js/order.js
+++ SBStoreWeb/wwwroot/js/order.js
@@ -1,17 +1,38 @@
 ﻿var datTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
 });
 
-function loadDataTable() {
-   datTable = $('#tblData').DataTable({
-       "ajax": { url: "/admin/order/getall" },
+function loadDataTable(status) {
+    datTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
-            { data: "id", "width": "25%" },
-            { data: "name", "width": "15%" },
-            { data: "phoneNumber", "width": "10%" },
-            { data: "appuser.email", "width": "15%" },
+            { data: "id", "width": "5%" },
+            { data: "name", "width": "25%" },
+            { data: "phoneNumber", "width": "20%" },
+            { data: "appuser.email", "width": "20%" },
             { data: "orderStatus", "width": "10%" },
             { data: "orderTotal", "width": "10%" },
             {
@@ -21,7 +42,7 @@ function loadDataTable() {
                      <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
                     </div>`
                 },
-                "width": "25%"
+                "width": "10%"
             }
         ]
     });
